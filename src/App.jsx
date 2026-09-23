5e2ca4c78e8224ce48d5a7020e9a4c299a76f864
deepak1234dp <deepak5698042@gmail.com>
2026-09-23 05:52:46 +0530

    first push

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function App() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <p>React without Vite.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+
+  entry: "./src/index.jsx",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+
+  devServer: {
+    port: 3000,
+    hot: true,
+    open: true,
+  },
+};
